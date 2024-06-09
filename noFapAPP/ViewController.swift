bf8b07f254dcb229f46383faa25f78a2c7507520
abayellow <abayellow@github.com>
2024-06-10 10:33:45 +1100

    Initial Commit

---
+++ noFapAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noFapAPP
+//
+//  Created by Alexander Abanshin on 10.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
